d6077300e1d91d1a2fb301ee12c670f468463505
leitong <leitongchen93@gmail.com>
2021-05-12 16:34:23 +0200

    Aggiunti gli album in html

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,37 @@
 new Vue({
     el: "#app",
     data: {
-        
+        albumsList: [],
     },
     methods: {
 
     },
+    mounted() {
+        let ajaxArrayLength = 1;
+
+        for (let i=0; i < ajaxArrayLength; i++) {
+
+            let ajaxList = []
+
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((resp) => {
+                
+                // console.log(resp.data.response.length);
+
+                ajaxArrayLength = resp.data.response.length;
+                console.log(ajaxArrayLength);
+
+                ajaxList = resp.data.response;
+                // console.log(ajaxList);
+
+                if (ajaxList.length === ajaxArrayLength) {
+                    this.albumsList = ajaxList
+                   
+                    console.log(this.albumsList);
+                }
+
+            })
+
+        }
+
+    }
 })
